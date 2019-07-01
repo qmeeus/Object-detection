65222dce15084e931c07d40201bd80b83b65d4bb
Quentin Meeus <qmeeus@gmail.com>
2019-07-02 01:54:36 +0200

    fix docker and scripts

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+xhost +local:docker
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run -m 4GB -it --rm --device=/dev/video0 -e DISPLAY=$DISPLAY \
+    -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
+    -e XAUTHORITY=$XAUTH -v ${PWD}:/lab \
+    realtime-object-detection:latest
+
+xhost -local:docker
