a78b55521502b6cdb29932f324025000f21fffba
Quentin Meeus <qmeeus@gmail.com>
2019-07-02 10:42:27 +0200

    reduce num workers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #src:
 python3 main.py \
-    --display 1 --output 1 --stream \
-    --num-workers 20 -q-size 150 \
+    --display 1 --output 1 \
+    --num-workers 2 -q-size 150 \
     --input-device rtmp://10.1.129.22/live/RamonStream
 
