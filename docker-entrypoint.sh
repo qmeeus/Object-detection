46132734ac949563c250e70567b066715aade425
quent <qmeeus@gmail.com>
2019-07-02 09:28:03 +0200

    entrypoint scripts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#src: rtmp://10.1.129.22/live/RamonStream
 python3 main.py \
     --display 1 --output 1 \
     --num-workers 20 -q-size 150 \
-    --input-device rtmp://10.1.129.22/live/RamonStream
+    --input-device test.mp4
 
