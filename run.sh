7d893f853eefea51495b5e6f15729c5395a52f01
Quentin Meeus <qmeeus@gmail.com>
2019-07-02 09:32:52 +0200

    remove device from docker run cmd

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run -m 4GB -it --rm --device=/dev/video0 -e DISPLAY=$DISPLAY \
-    -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH \
-    -e XAUTHORITY=$XAUTH -v $PWD:/home/patrick/app \
+docker run -m 4GB -it --rm \ 
+    # --device=/dev/video0 \ 
+    -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTH \
+    -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -v $PWD:/home/patrick/app \
     realtime-object-detection:latest #test.mp4
 
 #xhost -local:docker
